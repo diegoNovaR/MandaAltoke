b0a999f220c42e9007d4cdcede2be27fbbea92a0
Diego Nova <diegonova368@gmail.com>
2025-11-25 13:21:37 -0500

    Agregando inicio de fragmen en el main.kt

--- AppMandaAltoke/app/src/main/java/com/example/mandaaltoke/MainActivity.kt
+++ AppMandaAltoke/app/src/main/java/com/example/mandaaltoke/MainActivity.kt
@@ -10,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentContainer, InicioSesionFragment())
 
+            commit()
+        }
     }
 }
