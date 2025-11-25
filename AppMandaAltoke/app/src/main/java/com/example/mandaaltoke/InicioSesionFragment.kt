94bf38e985c03b11f6f4f4ceebd51928fe6427e0
Diego Nova <diegonova368@gmail.com>
2025-11-25 15:07:08 -0500

    Terminando de configurar Firebase

--- AppMandaAltoke/app/src/main/java/com/example/mandaaltoke/InicioSesionFragment.kt
+++ AppMandaAltoke/app/src/main/java/com/example/mandaaltoke/InicioSesionFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mandaaltoke
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,5 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 
 class InicioSesionFragment : Fragment(R.layout.fragment_inicio_sesion) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+    }
 }
