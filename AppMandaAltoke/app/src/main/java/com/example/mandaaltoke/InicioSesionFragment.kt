86e274cdb1cd6982ef94d5b99b0f026d4a877f1f
Diego Nova <diegonova368@gmail.com>
2025-11-26 00:59:40 -0500

    no funciona la conexion con firebase, manda errror

--- AppMandaAltoke/app/src/main/java/com/example/mandaaltoke/InicioSesionFragment.kt
+++ AppMandaAltoke/app/src/main/java/com/example/mandaaltoke/InicioSesionFragment.kt
@@ -1,14 +1,23 @@
 package com.example.mandaaltoke
 
-import android.widget.EditText
-import android.widget.Button
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.google.firebase.auth.FirebaseAuth
-
+import androidx.navigation.fragment.findNavController
 
 class InicioSesionFragment : Fragment(R.layout.fragment_inicio_sesion) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Botón "Crear empresa"
+        val btnCrear = view.findViewById<Button>(R.id.btnCrearCuenta)
+
+        btnCrear.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_inicioSesionFragment_to_crearEmpresaFragment
+            )
+        }
+    }
 }
